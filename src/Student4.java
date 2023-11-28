a81b382e6330ee4f57fdb4b548003df3284dcb22
imm103 <iwona.mucha000@gmail.com>
2023-11-28 16:17:31 +0100

    Added inserts of data

--- src/Student4.java
+++ src/Student4.java
@@ -6,5 +6,16 @@ public class Student4 {
         //Tworzenie obiektu klasy scanner do wczytywania danych z klawiatury
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Dodaj studenta: ");
+        //Komunikat proszący o wprowadzenie danych
+        System.out.print("Podaj imie: ");
+        //Wczytanie wprowadzonego tekstu do zmniennej
+        String imie = scanner.nextLine();
+        System.out.print("Podaj nazwisko: ");
+        String nazwisko = scanner.nextLine();
+        System.out.print("Podaj rok studiow: ");
+        String rokStudiow = scanner.nextLine();
+        System.out.print("Podaj kierunek: ");
+        String kierunek = scanner.nextLine();
     }
 }
